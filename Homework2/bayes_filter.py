0547292a645d1f254d5aa5af87ff8f0ade4fd2b8
David <dmagill89@gmail.com>
2023-01-24 19:53:54 -0800

    adding homework 2 file

---
+++ Homework2/bayes_filter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import matplotlib
+
+def main():
+    print("Bayes Filter")
+
+def control(beliefs):
+    print("calculate beliefs")
+
+def measurement():
+    print("calculate measurements")
+
+if __name__ == 'main':
+    main()
