b8c9bc95552f1e10a2aa45d3f3b921a444fe0eb9
David <dmagill89@gmail.com>
2023-02-14 19:15:30 -0800

    Adding homework5 first part

---
+++ Homework5/build/CMakeFiles/homework5.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework5.o.dir/homework5.cpp.o"
+  "homework5.o"
+  "homework5.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework5.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework5/homework5.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
+using namespace cv;
+using namespace cv::xfeatures2d;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class VideoBase {
+
+    public:
+        VideoBase(); // Default constructor
+
+        VideoBase(string file_name); // Overloaded constructor
+
+        void play(); // function to play video
+
+    private:
+        VideoCapture video;
+
+        string file_name;
+
+        void match(Mat image_template, Mat frame);
+};
+
+VideoBase::VideoBase() {
+    cout << "Object default constructor called" << endl;
+}
+
+VideoBase::VideoBase(string file_name) : video(file_name) {
+    cout << "Custom constructor called." << endl;
+    file_name = file_name;
+}
+
+void VideoBase::play() {
+    Mat valveTemplate = imread("../required_files/valve.png", IMREAD_GRAYSCALE);          // valve template for feature matching
+    Mat circuitTemplate = imread("../required_files/circuit.png", IMREAD_GRAYSCALE);      // circuit template or freature mathcing
+
+    if (!video.isOpened()) {
+        cout << "Error opening video stream of file." << endl;
+        exit(-1);
+    }
+
+    // loop over the frames of the video
+    while (video.isOpened()) {
+        Mat frame;
+
+        // wait for a new frame from file and store it into frame
+        video.read(frame);
+
+        if (frame.empty()) {
+            cout << "ERROR: Blank frame grabbed" << endl;
+            exit(-1);
+        }
+
+        match(valveTemplate, frame);
+        match(circuitTemplate, frame);
+
+        imshow("Frame", frame);
+
+        if (waitKey(5) >= 0) {
+            break;
+        }
+    }
+}
+
+void VideoBase::match(Mat image_template, Mat frame) {
+        Ptr<SIFT> detector = SIFT::create();
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+    detector->detectAndCompute(image_template, noArray(), keypoints_object, descriptors_object);
+
+    // Step 1: Detect the keypoints using SIFT dectector, compute descriptor
+    detector->detectAndCompute(frame, noArray(), keypoints_scene, descriptors_scene);
+
+    // Step 2: Matching descriptor vectors with FLANN based Matcher
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector<std::vector<DMatch>> knnMatches;
+    matcher->knnMatch(descriptors_object, descriptors_scene, knnMatches, 2);
+
+    // Filter matches using the Lowe's ratio test
+    const float ratioThreshold = 0.75f;
+    std::vector<DMatch> goodMacthes;
+
+    for (size_t i = 0; i < knnMatches.size(); i++) {
+        if (knnMatches[i][0].distance < ratioThreshold * knnMatches[i][1].distance) {
+            goodMacthes.push_back(knnMatches[i][0]);
+        }
+    }
+
+    // Draw Matches
+    // localize the object
+    std::vector<Point2f> object;
+    std::vector<Point2f> scene;
+
+    for (size_t i = 0; i < goodMacthes.size(); i++) {
+        // Get keypoints from good matches
+        object.push_back(keypoints_object[goodMacthes[i].queryIdx].pt);
+        scene.push_back(keypoints_scene[goodMacthes[i].trainIdx].pt);
+    }
+
+    Mat H = findHomography(object, scene, RANSAC);
+
+    // Get the corners from template image
+    std::vector<Point2f> objectCorners(4);
+    objectCorners[0] = Point2f(0, 0);
+    objectCorners[1] = Point2f((float) image_template.cols, 0);
+    objectCorners[2] = Point2f((float) image_template.cols, (float) image_template.rows);
+    objectCorners[3] = Point2f(0, (float) image_template.rows);
+
+    std::vector<Point2f> sceneCorners(4);
+    perspectiveTransform(objectCorners, sceneCorners, H);
+
+    // draw to frame
+    line(frame, sceneCorners[0], sceneCorners[1], Scalar(0, 0, 255), 4);
+    line(frame, sceneCorners[1], sceneCorners[2], Scalar(0, 0, 255), 4);
+    line(frame, sceneCorners[2], sceneCorners[3], Scalar(0, 0, 255), 4);
+    line(frame, sceneCorners[3], sceneCorners[0], Scalar(0, 0, 255), 4);
+}
+
+class ObjectDetection : VideoBase {
+
+    public:
+        ObjectDetection();
+        ~ObjectDetection();
+};
+
+int main(int argc, char **argv) {
+    string file_name;
+
+    cout << "Enter Filer Name: ";
+    cin >> file_name;
+    cout << file_name << endl;
+
+    VideoBase video_base(file_name);
+
+    video_base.play();
+
+    return 0;
+}
